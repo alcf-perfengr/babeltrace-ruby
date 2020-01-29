87740754d4e7699bfcc4d7d051edb02cd313de0b
Brice Videau <bvideau@anl.gov>
2020-01-29 14:33:40 -0600

    More use case.

--- test/test.rb
+++ test/test.rb
@@ -11,16 +11,20 @@ class BabeltraceTest < Minitest::Test
   def test_ctf
     c = Babeltrace::Context::new
     require 'babeltrace/ctf'
-    t = c.add_trace(path: "./trace-lud/ust/uid/1000/64-bit/")
-#    t = c.add_trace(path: "/home/videau/lttng-traces/my-userspace-opencl-session-20191020-154348/ust/uid/1000/64-bit/")
-    p t.get_path
+    traces = c.add_traces(path: "./trace-lud/")
+    p traces
     puts c.get_timestamp_begin.strftime("%Y-%m-%d %H:%M:%S.%9L %z")
     puts c.get_timestamp_end.strftime("%Y-%m-%d %H:%M:%S.%9L %z")
-    puts t.get_event_decl_list.collect(&:name)
+    event_names = {}
+    traces.each { |t|
+      event_names[t.handle_id] = t.get_event_decl_list.collect(&:name)
+    }
     it = c.iter_create
     it.each { |ev|
-      puts "#{ev.name}: #{ev.timestamp.strftime("%H:%M:%S.%9L")}"
-      Babeltrace::CTF::Scope.symbols.each { |sym|
+      #puts "#{ev.name}: #{ev.timestamp.strftime("%H:%M:%S.%9L")}"
+      puts "#{event_names[ev.handle_id][ev.id]}: #{ev.timestamp.strftime("%H:%M:%S.%9L")}"
+#      Babeltrace::CTF::Scope.symbols.each { |sym|
+      [:EVENT_FIELDS].each { |sym|
         defi = ev.top_level_scope(sym)
         next unless defi
         puts "\t#{sym}: #{defi.value}"
